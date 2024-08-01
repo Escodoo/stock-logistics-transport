a233cf22a71ad44e06ba2f6a21ffe99c2339d46f
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-08-01 15:04:01 -0300

    [ADD] tms_sale_order_pickup: add new module

---
+++ tms_sale_order_pickup/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Tms Sale Order Pickup",
+    "summary": """
+        TMS Sale Pickup Order""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-transport",
+    "depends": [
+        "tms_sale",
+        "tms_order_pickup",
+    ],
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "demo": [],
+}
