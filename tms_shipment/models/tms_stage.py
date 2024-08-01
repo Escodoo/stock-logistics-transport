97fd497829df9015542548c4aaf992c4bf99cca1
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-08-01 15:03:07 -0300

    [ADD] tms_shipment: add new module

---
+++ tms_shipment/models/tms_stage.py
@@ -0,0 +1,9 @@
+# Copyright 2024 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class TmsStage(models.Model):
+
+    _inherit = "tms.stage"
