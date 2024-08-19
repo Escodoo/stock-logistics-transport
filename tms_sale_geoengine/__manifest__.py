8ba9d4611115d588744c3a85131e5ba57b63aa1c
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-08-19 18:52:21 -0300

    [ADD] tms_sale_geoengine: add new module

---
+++ tms_sale_geoengine/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "TMS Sale Geoengine",
+    "summary": """
+        TMS Sale Geoengine""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-transport",
+    "depends": [
+        "tms_geoengine",
+        "tms_sale",
+    ],
+    "data": [
+        "views/tms_order.xml",
+    ],
+    "demo": [],
+}
